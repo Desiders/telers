e8920703d4e4f28e1a05dfeefb10fc9b0338f2e2
Desiders <47452083+Desiders@users.noreply.github.com>
2022-12-26 20:53:46 +0300

    🥅 Added some kind errors in `ErrorKind`

--- src/error/session.rs
+++ src/error/session.rs
@@ -1,11 +1,20 @@
 use super::telegram;
 
+use anyhow;
 use thiserror;
 
 #[derive(thiserror::Error, Debug)]
 pub enum ErrorKind {
+    /// Error while parsing JSON
     #[error(transparent)]
     Parse(#[from] serde_json::Error),
+    /// Error by Telegram API
     #[error(transparent)]
     Telegram(#[from] telegram::ErrorKind),
+    /// Error while sending request
+    #[error(transparent)]
+    Request(anyhow::Error),
+    /// Error while decoding response
+    #[error(transparent)]
+    Decode(anyhow::Error),
 }
