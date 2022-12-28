46fe63ec87b734c43575a4fbf3d9a9ebd0bb0242
Desiders <47452083+Desiders@users.noreply.github.com>
2022-12-28 19:45:10 +0300

    ♿️ Replaced error attributes`Request` and `Decode` to `Client`

--- src/error/session.rs
+++ src/error/session.rs
@@ -5,16 +5,13 @@ use thiserror;
 
 #[derive(thiserror::Error, Debug)]
 pub enum ErrorKind {
+    /// Error while sending request or decoding response
+    #[error(transparent)]
+    Client(anyhow::Error),
     /// Error while parsing JSON
     #[error(transparent)]
     Parse(#[from] serde_json::Error),
     /// Error by Telegram API
     #[error(transparent)]
     Telegram(#[from] telegram::ErrorKind),
-    /// Error while sending request
-    #[error(transparent)]
-    Request(anyhow::Error),
-    /// Error while decoding response
-    #[error(transparent)]
-    Decode(anyhow::Error),
 }
