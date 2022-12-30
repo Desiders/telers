bdeac4064c08fae4a00ae98466b5fd26198ec147
Desiders <47452083+Desiders@users.noreply.github.com>
2022-12-30 23:20:48 +0300

    ✨ Added `ServiceProvider` and `ToServiceProvider` traits

--- src/dispatcher/event/service.rs
+++ src/dispatcher/event/service.rs
@@ -4,7 +4,7 @@ mod base;
 mod boxed;
 mod fn_service;
 
-pub use base::{Service, ServiceFactory};
+pub use base::{Service, ServiceFactory, ServiceProvider, ToServiceProvider};
 pub use boxed::{factory, service, BoxFuture, BoxService, BoxServiceFactory};
 pub use fn_service::{
     fn_factory_config, fn_factory_no_config, fn_service, FnService, FnServiceConfig,
--- src/dispatcher/event/service/base.rs
+++ src/dispatcher/event/service/base.rs
@@ -1,4 +1,4 @@
-use std::future::Future;
+use std::{future::Future, sync::Arc};
 
 pub trait Service<Req> {
     /// Response given by the service
@@ -13,28 +13,6 @@ pub trait Service<Req> {
     /// Process the event and return the asynchronous response
     #[must_use]
     fn call(&self, req: Req) -> Self::Future;
-}
-
-pub trait ServiceFactory<Req> {
-    /// Response given by the service
-    type Response;
-
-    /// Error produced by the service when executing call
-    type Error;
-
-    /// Service factory configuration
-    type Config;
-
-    /// The kind of `Service` created by this factory
-    type Service: Service<Req, Response = Self::Response, Error = Self::Error>;
-
-    /// Errors potentially raised while building a service
-    type InitError;
-
-    /// Create and return a new service
-    /// # Errors
-    /// If the service cannot be created by the factory
-    fn new_service(&self, cfg: Self::Config) -> Result<Self::Service, Self::InitError>;
 }
 
 impl<'a, S, Req> Service<Req> for &'a S
@@ -61,4 +39,55 @@ where
     fn call(&self, req: Req) -> S::Future {
         (**self).call(req)
     }
+}
+
+pub trait ServiceFactory<Req> {
+    /// Response given by the service
+    type Response;
+
+    /// Error produced by the service when executing call
+    type Error;
+
+    /// Service factory configuration
+    type Config;
+
+    /// The kind of `Service` created by this factory
+    type Service: Service<Req, Response = Self::Response, Error = Self::Error>;
+
+    /// Errors potentially raised while building a service
+    type InitError;
+
+    /// Create and return a new service
+    /// # Errors
+    /// If the service cannot be created by the factory
+    fn new_service(&self, cfg: Self::Config) -> Result<Self::Service, Self::InitError>;
+}
+
+/// A marker trait, which means that it doesn't have any methods.
+/// It is used to mark service containers, which will be used to provide [`Service`]s.
+pub trait ServiceProvider {}
+
+impl<S> ServiceProvider for Arc<S> where S: ServiceProvider {}
+
+/// A trait that allows to convert structures, that contains [`ServiceFactory`]s, to [`ServiceProvider`],
+/// which will contains [`Service`]s instead of [`ServiceFactory`]s.
+/// In other words, it allows to build services from factories.
+pub trait ToServiceProvider {
+    /// Service factory configuration
+    type Config;
+
+    /// The provider to which the service will be converted
+    type ServiceProvider: ServiceProvider;
+
+    /// Errors potentially raised while building a service
+    type InitError;
+
+    /// Convert the service factory to the service.
+    /// Call [`ServiceFactory::new_service`] for each factory and return the [`ServiceProvider`].
+    /// # Errors
+    /// If the service cannot be created by the factory
+    fn to_service_provider(
+        self,
+        cfg: Self::Config,
+    ) -> Result<Self::ServiceProvider, Self::InitError>;
 }
