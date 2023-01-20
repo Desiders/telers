6b7c0f556a3f2b688076ae183edd337cf31c4268
Desiders <47452083+Desiders@users.noreply.github.com>
2023-01-20 23:22:48 +0300

    📝 Updated doc for some telegram types

--- src/types/input_media_photo.rs
+++ src/types/input_media_photo.rs
@@ -11,7 +11,7 @@ pub struct InputMediaPhoto<'a> {
     /// Type of the result, must be *photo*
     #[serde(rename = "type")]
     pub media_type: String,
-    /// File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), pass an HTTP URL for Telegram to get a file from the Internet, or pass 'attach://<file_attach_name>' to upload a new one using multipart/form-data under <file_attach_name> name. :ref:`More information on Sending Files » <sending-files>`
+    /// File to send. Pass a file_id to send a file that exists on the Telegram servers (recommended), pass an HTTP URL for Telegram to get a file from the Internet, or pass 'attach://<file_attach_name>' to upload a new one using `multipart/form-data` under <file_attach_name> name. `More information on Sending Files <https://core.telegram.org/bots/api#sending-files>`.
     pub media: InputFile<'a>,
     /// *Optional*. Caption of the photo to be sent, 0-1024 characters after entities parsing
     pub caption: Option<String>,
