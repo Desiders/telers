add7c5692a2b8209c7df5815631cb6ac287ee7b1
Desiders <47452083+Desiders@users.noreply.github.com>
2023-02-07 19:33:35 +0300

    🎨 Changed error type and underscore to a more suitable for `CommandObject`

--- src/extract/filters.rs
+++ src/extract/filters.rs
@@ -1,32 +1,29 @@
 use crate::{
-    client::Bot, context::Context, error::app, extract::FromEventAndContext,
+    client::Bot, context::Context, error::ExtractionError, extract::FromEventAndContext,
     filters::command::CommandObject, types::Update,
 };
 
-use futures::future::{err, ok, Ready};
 use std::sync::Arc;
 
 impl FromEventAndContext for CommandObject {
-    type Error = app::ErrorKind;
-    type Future = Ready<Result<Self, Self::Error>>;
+    type Error = ExtractionError;
 
-    fn extract(_: Arc<Bot>, _: Arc<Update>, context: Arc<Context>) -> Self::Future {
-        context.get("command").map_or(
-            err(app::ErrorKind::ExtractError(
-                "Key `command` not found in the context".into(),
-            )),
-            |command| {
-                command.downcast_ref::<CommandObject>().map_or(
-                    err(app::ErrorKind::ExtractError(
-                        format!(
-                            "Failed to downcast command, got `{command:?}` instead `CommandObject`"
-                        )
-                        .into(),
-                    )),
-                    |command| ok(command.clone()),
-                )
+    fn extract(
+        _bot: Arc<Bot>,
+        _update: Arc<Update>,
+        context: Arc<Context>,
+    ) -> Result<Self, Self::Error> {
+        match context.get("command") {
+            Some(command) => match command.downcast_ref::<CommandObject>() {
+                Some(command) => Ok((*command).clone()),
+                None => Err(ExtractionError::new(format!(
+                    "Failed to downcast command, got `{command:?}` instead `CommandObject`"
+                ))),
             },
-        )
+            None => Err(ExtractionError::new(
+                "Key `command` not found in the context",
+            )),
+        }
     }
 }
 
