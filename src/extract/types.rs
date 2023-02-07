8bdfbb995cb3c923573f1cfc47199d9cf20e1a88
Desiders <47452083+Desiders@users.noreply.github.com>
2023-02-07 19:29:58 +0300

    ✅ Added tests and replaced underscore to a more explicit analogue in extractor

--- src/extract/types.rs
+++ src/extract/types.rs
@@ -1,6 +1,7 @@
 use crate::{
     client::Bot,
     context::Context,
+    error::ConvertUpdateToTypeError,
     extract::FromEventAndContext,
     types::{
         CallbackQuery, ChatJoinRequest, ChatMemberUpdated, ChosenInlineResult, InlineQuery,
@@ -8,156 +9,200 @@ use crate::{
     },
 };
 
-use futures::future::{ok, Ready};
 use std::{convert::Infallible, sync::Arc};
 
 /// To be able to use [`Bot`] in handler arguments
 impl FromEventAndContext for Bot {
     type Error = Infallible;
-    type Future = Ready<Result<Self, Self::Error>>;
 
-    fn extract(bot: Arc<Bot>, _: Arc<Update>, _: Arc<Context>) -> Self::Future {
-        ok((*bot).clone())
+    fn extract(
+        bot: Arc<Bot>,
+        _update: Arc<Update>,
+        _context: Arc<Context>,
+    ) -> Result<Self, Self::Error> {
+        Ok((*bot).clone())
     }
 }
 
 /// To be able to use [`Arc<Bot>`] in handler arguments
 impl FromEventAndContext for Arc<Bot> {
     type Error = Infallible;
-    type Future = Ready<Result<Self, Self::Error>>;
 
-    fn extract(bot: Arc<Bot>, _: Arc<Update>, _: Arc<Context>) -> Self::Future {
-        ok(bot)
+    fn extract(
+        bot: Arc<Bot>,
+        _update: Arc<Update>,
+        _context: Arc<Context>,
+    ) -> Result<Self, Self::Error> {
+        Ok(bot)
     }
 }
 
 /// To be able to use [`Update`] in handler arguments
 impl FromEventAndContext for Update {
     type Error = Infallible;
-    type Future = Ready<Result<Self, Self::Error>>;
 
-    fn extract(_: Arc<Bot>, update: Arc<Update>, _: Arc<Context>) -> Self::Future {
-        ok((*update).clone())
+    fn extract(
+        _bot: Arc<Bot>,
+        update: Arc<Update>,
+        _context: Arc<Context>,
+    ) -> Result<Self, Self::Error> {
+        Ok((*update).clone())
     }
 }
 
 /// To be able to use [`Arc<Update>`] in handler arguments
 impl FromEventAndContext for Arc<Update> {
     type Error = Infallible;
-    type Future = Ready<Result<Self, Self::Error>>;
 
-    fn extract(_: Arc<Bot>, update: Arc<Update>, _: Arc<Context>) -> Self::Future {
-        ok(update)
+    fn extract(
+        _bot: Arc<Bot>,
+        update: Arc<Update>,
+        _context: Arc<Context>,
+    ) -> Result<Self, Self::Error> {
+        Ok(update)
     }
 }
 
 /// To be able to use [`Context`] in handler arguments
 impl FromEventAndContext for Arc<Context> {
     type Error = Infallible;
-    type Future = Ready<Result<Self, Self::Error>>;
 
-    fn extract(_: Arc<Bot>, _: Arc<Update>, context: Arc<Context>) -> Self::Future {
-        ok(context)
+    fn extract(
+        _bot: Arc<Bot>,
+        _update: Arc<Update>,
+        context: Arc<Context>,
+    ) -> Result<Self, Self::Error> {
+        Ok(context)
     }
 }
 
 /// To be able to use [`Message`] in handler arguments
 impl FromEventAndContext for Message {
-    type Error = Infallible;
-    type Future = Ready<Result<Self, Self::Error>>;
+    type Error = ConvertUpdateToTypeError;
 
-    fn extract(_: Arc<Bot>, update: Arc<Update>, _: Arc<Context>) -> Self::Future {
-        ok(<Message as From<_>>::from((*update).clone()))
+    fn extract(
+        _bot: Arc<Bot>,
+        update: Arc<Update>,
+        _context: Arc<Context>,
+    ) -> Result<Self, Self::Error> {
+        Message::try_from((*update).clone())
     }
 }
 
 /// To be able to use [`CallbackQuery`] in handler arguments
 impl FromEventAndContext for CallbackQuery {
-    type Error = Infallible;
-    type Future = Ready<Result<Self, Self::Error>>;
+    type Error = ConvertUpdateToTypeError;
 
-    fn extract(_: Arc<Bot>, update: Arc<Update>, _: Arc<Context>) -> Self::Future {
-        ok(<CallbackQuery as From<_>>::from((*update).clone()))
+    fn extract(
+        _bot: Arc<Bot>,
+        update: Arc<Update>,
+        _context: Arc<Context>,
+    ) -> Result<Self, Self::Error> {
+        CallbackQuery::try_from((*update).clone())
     }
 }
 
 /// To be able to use [`ChosenInlineResult`] in handler arguments
 impl FromEventAndContext for ChosenInlineResult {
-    type Error = Infallible;
-    type Future = Ready<Result<Self, Self::Error>>;
+    type Error = ConvertUpdateToTypeError;
 
-    fn extract(_: Arc<Bot>, update: Arc<Update>, _: Arc<Context>) -> Self::Future {
-        ok(<ChosenInlineResult as From<_>>::from((*update).clone()))
+    fn extract(
+        _bot: Arc<Bot>,
+        update: Arc<Update>,
+        _context: Arc<Context>,
+    ) -> Result<Self, Self::Error> {
+        ChosenInlineResult::try_from((*update).clone())
     }
 }
 
 /// To be able to use [`ShippingQuery`] in handler arguments
 impl FromEventAndContext for ShippingQuery {
-    type Error = Infallible;
-    type Future = Ready<Result<Self, Self::Error>>;
+    type Error = ConvertUpdateToTypeError;
 
-    fn extract(_: Arc<Bot>, update: Arc<Update>, _: Arc<Context>) -> Self::Future {
-        ok(<ShippingQuery as From<_>>::from((*update).clone()))
+    fn extract(
+        _bot: Arc<Bot>,
+        update: Arc<Update>,
+        _context: Arc<Context>,
+    ) -> Result<Self, Self::Error> {
+        ShippingQuery::try_from((*update).clone())
     }
 }
 
 /// To be able to use [`PreCheckoutQuery`] in handler arguments
 impl FromEventAndContext for PreCheckoutQuery {
-    type Error = Infallible;
-    type Future = Ready<Result<Self, Self::Error>>;
+    type Error = ConvertUpdateToTypeError;
 
-    fn extract(_: Arc<Bot>, update: Arc<Update>, _: Arc<Context>) -> Self::Future {
-        ok(<PreCheckoutQuery as From<_>>::from((*update).clone()))
+    fn extract(
+        _bot: Arc<Bot>,
+        update: Arc<Update>,
+        _context: Arc<Context>,
+    ) -> Result<Self, Self::Error> {
+        PreCheckoutQuery::try_from((*update).clone())
     }
 }
 
 /// To be able to use [`PollAnswer`] in handler arguments
 impl FromEventAndContext for PollAnswer {
-    type Error = Infallible;
-    type Future = Ready<Result<Self, Self::Error>>;
+    type Error = ConvertUpdateToTypeError;
 
-    fn extract(_: Arc<Bot>, update: Arc<Update>, _: Arc<Context>) -> Self::Future {
-        ok(<PollAnswer as From<_>>::from((*update).clone()))
+    fn extract(
+        _bot: Arc<Bot>,
+        update: Arc<Update>,
+        _context: Arc<Context>,
+    ) -> Result<Self, Self::Error> {
+        PollAnswer::try_from((*update).clone())
     }
 }
 
 /// To be able to use [`ChatMemberUpdated`] in handler arguments
 impl FromEventAndContext for ChatMemberUpdated {
-    type Error = Infallible;
-    type Future = Ready<Result<Self, Self::Error>>;
+    type Error = ConvertUpdateToTypeError;
 
-    fn extract(_: Arc<Bot>, update: Arc<Update>, _: Arc<Context>) -> Self::Future {
-        ok(<ChatMemberUpdated as From<_>>::from((*update).clone()))
+    fn extract(
+        _bot: Arc<Bot>,
+        update: Arc<Update>,
+        _context: Arc<Context>,
+    ) -> Result<Self, Self::Error> {
+        ChatMemberUpdated::try_from((*update).clone())
     }
 }
 
 /// To be able to use [`ChatJoinRequest`] in handler arguments
 impl FromEventAndContext for ChatJoinRequest {
-    type Error = Infallible;
-    type Future = Ready<Result<Self, Self::Error>>;
+    type Error = ConvertUpdateToTypeError;
 
-    fn extract(_: Arc<Bot>, update: Arc<Update>, _: Arc<Context>) -> Self::Future {
-        ok(<ChatJoinRequest as From<_>>::from((*update).clone()))
+    fn extract(
+        _bot: Arc<Bot>,
+        update: Arc<Update>,
+        _context: Arc<Context>,
+    ) -> Result<Self, Self::Error> {
+        ChatJoinRequest::try_from((*update).clone())
     }
 }
 
 /// To be able to use [`InlineQuery`] in handler arguments
 impl FromEventAndContext for InlineQuery {
-    type Error = Infallible;
-    type Future = Ready<Result<Self, Self::Error>>;
+    type Error = ConvertUpdateToTypeError;
 
-    fn extract(_: Arc<Bot>, update: Arc<Update>, _: Arc<Context>) -> Self::Future {
-        ok(<InlineQuery as From<_>>::from((*update).clone()))
+    fn extract(
+        _bot: Arc<Bot>,
+        update: Arc<Update>,
+        _context: Arc<Context>,
+    ) -> Result<Self, Self::Error> {
+        InlineQuery::try_from((*update).clone())
     }
 }
 
 /// To be able to use [`Poll`] in handler arguments
 impl FromEventAndContext for Poll {
-    type Error = Infallible;
-    type Future = Ready<Result<Self, Self::Error>>;
+    type Error = ConvertUpdateToTypeError;
 
-    fn extract(_: Arc<Bot>, update: Arc<Update>, _: Arc<Context>) -> Self::Future {
-        ok(<Poll as From<_>>::from((*update).clone()))
+    fn extract(
+        _bot: Arc<Bot>,
+        update: Arc<Update>,
+        _context: Arc<Context>,
+    ) -> Result<Self, Self::Error> {
+        Poll::try_from((*update).clone())
     }
 }
 
@@ -166,8 +211,16 @@ mod tests {
     use super::*;
     use crate::dispatcher::event::telegram::handler::Handler;
 
+    fn inner_extract<T: FromEventAndContext>(
+        bot: Arc<Bot>,
+        update: Arc<Update>,
+        context: Arc<Context>,
+    ) -> Result<T, T::Error> {
+        T::extract(Arc::clone(&bot), Arc::clone(&update), Arc::clone(&context))
+    }
+
     #[test]
-    fn test_types_extract() {
+    fn test_impl_extract_types() {
         fn assert_impl_handler<T: FromEventAndContext>(_: impl Handler<T>) {}
 
         assert_impl_handler(|_: Bot| async { unreachable!() });
@@ -185,5 +238,201 @@ mod tests {
         assert_impl_handler(|_: ChatJoinRequest| async { unreachable!() });
         assert_impl_handler(|_: InlineQuery| async { unreachable!() });
         assert_impl_handler(|_: Poll| async { unreachable!() });
+    }
+
+    #[test]
+    fn test_extract() {
+        let bot = Arc::new(Bot::default());
+        let update = Arc::new(Update::default());
+        let context = Arc::new(Context::default());
+
+        inner_extract::<Bot>(Arc::clone(&bot), Arc::clone(&update), Arc::clone(&context)).unwrap();
+        inner_extract::<Arc<Bot>>(Arc::clone(&bot), Arc::clone(&update), Arc::clone(&context))
+            .unwrap();
+        inner_extract::<Update>(Arc::clone(&bot), Arc::clone(&update), Arc::clone(&context))
+            .unwrap();
+        inner_extract::<Arc<Update>>(Arc::clone(&bot), Arc::clone(&update), Arc::clone(&context))
+            .unwrap();
+        inner_extract::<Arc<Context>>(Arc::clone(&bot), Arc::clone(&update), Arc::clone(&context))
+            .unwrap();
+
+        inner_extract::<Message>(Arc::clone(&bot), Arc::clone(&update), Arc::clone(&context))
+            .unwrap_err();
+        inner_extract::<CallbackQuery>(Arc::clone(&bot), Arc::clone(&update), Arc::clone(&context))
+            .unwrap_err();
+        inner_extract::<ChosenInlineResult>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap_err();
+        inner_extract::<ShippingQuery>(Arc::clone(&bot), Arc::clone(&update), Arc::clone(&context))
+            .unwrap_err();
+        inner_extract::<PreCheckoutQuery>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap_err();
+        inner_extract::<PollAnswer>(Arc::clone(&bot), Arc::clone(&update), Arc::clone(&context))
+            .unwrap_err();
+        inner_extract::<ChatMemberUpdated>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap_err();
+        inner_extract::<ChatJoinRequest>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap_err();
+        inner_extract::<InlineQuery>(Arc::clone(&bot), Arc::clone(&update), Arc::clone(&context))
+            .unwrap_err();
+        inner_extract::<Poll>(Arc::clone(&bot), Arc::clone(&update), Arc::clone(&context))
+            .unwrap_err();
+
+        assert!(inner_extract::<Option<Message>>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap()
+        .is_none());
+        assert!(inner_extract::<Option<CallbackQuery>>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap()
+        .is_none());
+        assert!(inner_extract::<Option<ChosenInlineResult>>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap()
+        .is_none());
+        assert!(inner_extract::<Option<ShippingQuery>>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap()
+        .is_none());
+        assert!(inner_extract::<Option<PreCheckoutQuery>>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap()
+        .is_none());
+        assert!(inner_extract::<Option<PollAnswer>>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap()
+        .is_none());
+        assert!(inner_extract::<Option<ChatMemberUpdated>>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap()
+        .is_none());
+        assert!(inner_extract::<Option<ChatJoinRequest>>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap()
+        .is_none());
+        assert!(inner_extract::<Option<InlineQuery>>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap()
+        .is_none());
+        assert!(inner_extract::<Option<Poll>>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context)
+        )
+        .unwrap()
+        .is_none());
+
+        inner_extract::<Result<Message, ConvertUpdateToTypeError>>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap()
+        .unwrap_err();
+        inner_extract::<Result<CallbackQuery, ConvertUpdateToTypeError>>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap()
+        .unwrap_err();
+        inner_extract::<Result<ChosenInlineResult, ConvertUpdateToTypeError>>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap()
+        .unwrap_err();
+        inner_extract::<Result<ShippingQuery, ConvertUpdateToTypeError>>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap()
+        .unwrap_err();
+        inner_extract::<Result<PreCheckoutQuery, ConvertUpdateToTypeError>>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap()
+        .unwrap_err();
+        inner_extract::<Result<PollAnswer, ConvertUpdateToTypeError>>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap()
+        .unwrap_err();
+        inner_extract::<Result<ChatMemberUpdated, ConvertUpdateToTypeError>>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap()
+        .unwrap_err();
+        inner_extract::<Result<ChatJoinRequest, ConvertUpdateToTypeError>>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap()
+        .unwrap_err();
+        inner_extract::<Result<InlineQuery, ConvertUpdateToTypeError>>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap()
+        .unwrap_err();
+        inner_extract::<Result<Poll, ConvertUpdateToTypeError>>(
+            Arc::clone(&bot),
+            Arc::clone(&update),
+            Arc::clone(&context),
+        )
+        .unwrap()
+        .unwrap_err();
     }
 }
