639d68e1f12b0ec2a84ac6cb1d2b9a3fdc7403a4
Desiders <47452083+Desiders@users.noreply.github.com>
2023-03-22 19:24:30 +0300

    Add fsm features

--- src/fsm.rs
+++ src/fsm.rs
@@ -1,7 +1,10 @@
 pub mod context;
 pub mod storage;
 
+#[cfg(feature = "redis-storage")]
 pub use storage::{
-    DefaultKeyBuilder as RedisDefaultKeyBuilder, Memory as MemoryStorage, Redis as RedisStorage,
-    RedisStorageError,
+    DefaultKeyBuilder as RedisDefaultKeyBuilder, Redis as RedisStorage, RedisStorageError,
 };
+
+#[cfg(feature = "memory-storage")]
+pub use storage::Memory as MemoryStorage;
--- src/fsm/storage.rs
+++ src/fsm/storage.rs
@@ -1,8 +1,12 @@
 pub mod base;
+#[cfg(feature = "memory-storage")]
 pub mod memory;
+#[cfg(feature = "redis-storage")]
 pub mod redis;
 
+#[cfg(feature = "redis-storage")]
 pub use self::redis::{DefaultKeyBuilder, Redis, StorageError as RedisStorageError};
 #[allow(clippy::module_name_repetitions)]
 pub use base::{Storage, StorageKey};
+#[cfg(feature = "memory-storage")]
 pub use memory::Memory;
