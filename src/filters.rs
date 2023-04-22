b20f1483c80692785fc9ffd1053bc88e825a0eab
Desiders <47452083+Desiders@users.noreply.github.com>
2023-04-22 11:52:51 +0300

    Add new `ContentType` filter

--- src/filters.rs
+++ src/filters.rs
@@ -1,9 +1,11 @@
 pub mod base;
 pub mod command;
+pub mod content_type;
 pub mod state;
 pub mod text;
 
 pub use base::Filter;
 pub use command::{Command, CommandObject};
+pub use content_type::ContentType;
 pub use state::{State, StateType};
 pub use text::Text;
---
+++ src/filters/content_type.rs
@@ -0,0 +1,68 @@
+use super::base::Filter;
+
+use crate::{client::Bot, context::Context, enums::ContentType as ContentTypeEnum, types::Update};
+
+use async_trait::async_trait;
+
+#[derive(Debug, Clone)]
+pub struct ContentType {
+    content_types: Vec<ContentTypeEnum>,
+}
+
+impl ContentType {
+    pub fn one(content_type: impl Into<ContentTypeEnum>) -> Self {
+        Self {
+            content_types: vec![content_type.into()],
+        }
+    }
+
+    pub fn many<T, I>(content_types: I) -> Self
+    where
+        T: Into<ContentTypeEnum>,
+        I: IntoIterator<Item = T>,
+    {
+        Self {
+            content_types: content_types.into_iter().map(Into::into).collect(),
+        }
+    }
+}
+
+impl ContentType {
+    pub fn validate_content_type<T>(&self, content_type: T) -> bool
+    where
+        T: Into<ContentTypeEnum>,
+    {
+        let content_type = content_type.into();
+
+        self.content_types
+            .iter()
+            .any(|allowed_content_type| allowed_content_type == &content_type)
+    }
+}
+
+#[async_trait]
+impl<Client> Filter<Client> for ContentType {
+    async fn check(&self, _bot: &Bot<Client>, update: &Update, _context: &Context) -> bool {
+        update.message.as_ref().map_or(false, |message| {
+            self.validate_content_type(message.content_type())
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_content_type() {
+        let filter = ContentType::many([ContentTypeEnum::Text, ContentTypeEnum::Photo]);
+
+        assert!(filter.validate_content_type(ContentTypeEnum::Text));
+        assert!(filter.validate_content_type(ContentTypeEnum::Photo));
+        assert!(!filter.validate_content_type(ContentTypeEnum::Audio));
+
+        assert!(filter.validate_content_type("text"));
+        assert!(filter.validate_content_type("photo"));
+        assert!(!filter.validate_content_type("audio"));
+    }
+}
