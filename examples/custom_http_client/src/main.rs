a4ee01eb8cf3986af9891479c3c035fcf0a7a562
Desiders <47452083+Desiders@users.noreply.github.com>
2023-05-06 21:17:12 +0300

    Change custom http client example

--- examples/custom_http_client/src/main.rs
+++ examples/custom_http_client/src/main.rs
@@ -1,6 +1,6 @@
 //! This example shows how to use in handler custom client.
 //!
-//! Usually you don't need to use custom client, because [`telers`] provides default client, which is [`Reqwest`],
+//! Usually you don't need to use custom client, because [`telers`] provides default client,
 //! but if you want to use custom client, you can do it by using [`Bot::with_client`] method and use it in handlers.
 //!
 //! You can use any client, which implements [`Session`] trait and use it in handlers:
@@ -22,16 +22,56 @@
 //! RUST_LOG={log_level} BOT_TOKEN={your_bot_token} cargo run --bin custom_http_client
 //! ```
 
+use std::borrow::Cow;
 use telers::{
-    client::{Bot, Reqwest, Session},
+    client::{session::ClientResponse, telegram, Bot, Session},
     dispatcher::{
         event::{telegram::HandlerResult, EventReturn, ToServiceProvider as _},
         Dispatcher, Router,
     },
     enums::UpdateType,
-    methods::CopyMessage,
+    methods::{CopyMessage, TelegramMethod},
     types::Message,
 };
+
+use async_trait::async_trait;
+
+#[derive(Clone)]
+struct CustomClient {
+    api: Cow<'static, telegram::APIServer>,
+}
+
+impl Default for CustomClient {
+    fn default() -> Self {
+        Self {
+            api: Cow::Borrowed(&telegram::PRODUCTION),
+        }
+    }
+}
+
+#[async_trait]
+impl Session for CustomClient {
+    fn api(&self) -> &telegram::APIServer {
+        &self.api
+    }
+
+    async fn send_request<Client, T>(
+        &self,
+        _bot: &Bot<Client>,
+        _method: &T,
+        _timeout: Option<f32>,
+    ) -> Result<ClientResponse, anyhow::Error>
+    where
+        Client: Session,
+        T: TelegramMethod + Send + Sync,
+        T::Method: Send + Sync,
+    {
+        unimplemented!(
+            "Send request is not implemented for custom client. \
+            You can use default client or implement it for your custom client."
+        )
+    }
+}
 
 async fn echo_handler(bot: Bot<impl Session>, message: Message) -> HandlerResult {
     bot.send(
@@ -51,7 +91,7 @@ async fn main() {
         panic!("BOT_TOKEN env variable is not set!");
     };
 
-    let bot = Bot::with_client(bot_token, Reqwest::default());
+    let bot = Bot::with_client(bot_token, CustomClient::default());
 
     let mut router = Router::new("main");
     router.message.register(echo_handler);
