8a03187d4f0a2dff43c79c5829e22ef8d9e2568b
Desiders <abc_desi@proton.me>
2024-02-12 18:22:09 +0300

    Add bot module docs

--- telers/src/client/bot.rs
+++ telers/src/client/bot.rs
@@ -1,3 +1,39 @@
+//! This module contains the [`Bot`] structure that represents a bot with its token and ID,
+//! it also contains client for sending requests to Telegram API.
+//!
+//! # Notes
+//!
+//! This structure is cheap to clone, because it contains only [`String`], [`i64`] fields and a client.
+//! Default client is [`Reqwest`], which also is cheap to clone.
+//!
+//! You can use custom client by using [`Bot::with_client`] method.
+//!
+//! # Examples
+//! ```rust
+//! use telers::{Bot, methods::SendMessage};
+//!
+//! async fn call_method(bot: Bot) {
+//!     let chat_id = 1;
+//!     let text = "Hello, world!";
+//!
+//!     let _ = bot.send(SendMessage::new(chat_id, text)).await;
+//! }
+//! ```
+//!
+//! You also can use [`Bot::send_with_timeout`] method to send requests with timeout:
+//!
+//! ```rust
+//! use telers::{Bot, methods::SendMessage};
+//!
+//! async fn call_method(bot: Bot) {
+//!     let chat_id = 1;
+//!     let text = "Hello, world!";
+//!     let timeout = 10.0; // 10 seconds
+//!
+//!     let _ = bot.send_with_timeout(SendMessage::new(chat_id, text), timeout).await;
+//! }
+//! ```
+
 use super::{session::base::Session, Reqwest};
 
 use crate::{errors::SessionErrorKind, methods::TelegramMethod, utils::token};
@@ -5,7 +41,15 @@ use crate::{errors::SessionErrorKind, methods::TelegramMethod, utils::token};
 use std::fmt::{self, Debug, Display, Formatter};
 use tracing::instrument;
 
-/// Represents a bot with a token for getting updates and sending requests to Telegram API
+/// Represents a bot with its token and ID, also contains client for sending requests to Telegram API.
+/// # Notes
+/// This structure is cheap to clone, because it contains only [`String`], [`i64`] fields and a client.
+///
+/// Default client is [`Reqwest`], which also is cheap to clone.
+///
+/// You can use custom client by using [`Bot::with_client`] method.
+///
+/// Check [module docs](crate::client::bot) for examples.
 #[derive(Clone, Default)]
 pub struct Bot<Client: ?Sized = Reqwest> {
     /// Bot token, which is used to receive updates and send requests to the Telegram API
@@ -59,7 +103,12 @@ impl<Client> Debug for Bot<Client> {
 
 impl<Client> Display for Bot<Client> {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        f.write_str(&self.hidden_token)
+        // Displaying only bot id and hidden token, because the token is sensitive data
+        write!(
+            f,
+            "Bot {{ bot_id: {}, token: {} }}",
+            self.bot_id, self.hidden_token,
+        )
     }
 }
 
@@ -70,7 +119,10 @@ impl<Client: Session> Bot<Client> {
     /// # Errors
     /// - If the request cannot be send or decoded
     /// - If the response cannot be parsed
-    /// - If the response represents an telegram api error
+    /// - If the response represents an Telegram API error
+    /// # Notes
+    /// This method uses default timeout for requests, which is 30 seconds.
+    /// If you want to use custom timeout, use [`Bot::send_with_timeout`] method.
     #[instrument(skip(self, method))]
     pub async fn send<T, TRef>(&self, method: TRef) -> Result<T::Return, SessionErrorKind>
     where
@@ -90,8 +142,10 @@ impl<Client: Session> Bot<Client> {
     /// # Errors
     /// - If the request cannot be send or decoded
     /// - If the response cannot be parsed
-    /// - If the response represents an telegram api error
-    #[instrument(skip(self, method, request_timeout))]
+    /// - If the response represents an Telegram API error
+    /// # Notes
+    /// This method uses passed timeout for requests.
+    /// If you want to use default timeout, use [`Bot::send`] method.
     pub async fn send_with_timeout<T, TRef>(
         &self,
         method: TRef,
