5867fa1df705e02f42a6f64507a1ad7f93dff3d8
Desiders <abc_desi@proton.me>
2024-11-01 12:13:55 +0300

    Add `prize_star_count` for types

--- telers/src/types/giveaway_created.rs
+++ telers/src/types/giveaway_created.rs
@@ -4,4 +4,7 @@ use serde::{Deserialize, Serialize};
 /// # Documentation
 /// <https://core.telegram.org/bots/api#giveawaycreated>
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, Deserialize, Serialize)]
-pub struct GiveawayCreated {}
+pub struct GiveawayCreated {
+    /// The number of Telegram Stars to be split between giveaway winners; for Telegram Star giveaways only
+    pub prize_star_count: i64,
+}
