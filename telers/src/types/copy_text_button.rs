8f77851870acc418ab5e5f5228b168b9541c6349
Desiders <abc_desi@proton.me>
2024-11-01 13:56:52 +0300

    Add `CopyTextButton` type and `copy_text` in `Message`

---
+++ telers/src/types/copy_text_button.rs
@@ -0,0 +1,25 @@
+use serde::{Deserialize, Serialize};
+
+/// This object represents an inline keyboard button that copies specified text to the clipboard.
+/// # Documentation
+/// <https://core.telegram.org/bots/api#copytextbutton>
+#[derive(Debug, Clone, Hash, PartialEq, Eq, Deserialize, Serialize)]
+pub struct CopyTextButton {
+    /// The text to be copied to the clipboard; 1-256 characters
+    pub text: String,
+}
+
+impl CopyTextButton {
+    #[must_use]
+    pub fn new(text: impl Into<String>) -> Self {
+        Self { text: text.into() }
+    }
+
+    #[must_use]
+    pub fn text(self, val: impl Into<String>) -> Self {
+        Self {
+            text: val.into(),
+            ..self
+        }
+    }
+}
