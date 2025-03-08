0037a13d10d3dbfbe22ae4c7efd33efe3f9537cb
Desiders <abc_desi@proton.me>
2025-03-08 03:35:44 +0300

    Remove docs

--- telers/src/extensions.rs
+++ telers/src/extensions.rs
@@ -1,5 +1,5 @@
 //! [`Extension`] is a type that is used to transmit data between processing-units when propagating an event.
-//! [`Extensions`] creates at the start of the event propagation by [`Dispatcher`] and pass to every processing-unit.
+//! Extensions creates at the start of the event propagation by [`Dispatcher`] and pass to every processing-unit.
 //! Processing-units can add their own data to extension and use data from extension that was added by others.
 //!
 //! Modify extensions in outer middlewares if you need to pass some data to next outer/inner middlewares or to filters.
@@ -51,16 +51,6 @@ impl Extensions {
         Self { map: None }
     }
 
-    /// Insert a type into this [`Extensions`].
-    /// If the map did not have this key present, [`None`] is returned.
-    /// If a extension of this type already existed, it will be returned and replaced with the new one.
-    /// # Examples
-    /// ```
-    /// let mut ext = Extensions::new();
-    /// assert!(ext.insert(5i32).is_none());
-    /// assert!(ext.insert(4u8).is_none());
-    /// assert_eq!(ext.insert(9i32), Some(5i32));
-    /// ```
     pub fn insert<T: Clone + Send + Sync + 'static>(&mut self, val: T) -> Option<T> {
         self.map
             .get_or_insert_with(Box::default)
@@ -68,15 +58,6 @@ impl Extensions {
             .and_then(|boxed| boxed.into_any().downcast().ok().map(|boxed| *boxed))
     }
 
-    /// Get a reference to a type previously inserted on this [`Extensions`]
-    /// # Examples
-    /// ```
-    /// let mut ext = Extensions::new();
-    /// assert!(ext.get::<i32>().is_none());
-    /// ext.insert(5i32);
-    ///
-    /// assert_eq!(ext.get::<i32>(), Some(&5i32));
-    /// ```
     #[must_use]
     pub fn get<T: 'static>(&self) -> Option<&T> {
         self.map
@@ -85,15 +66,6 @@ impl Extensions {
             .and_then(|boxed| (**boxed).as_any().downcast_ref())
     }
 
-    /// Get a mutable reference to a type previously inserted on this [`Extensions`]
-    /// # Examples
-    /// ```
-    /// let mut ext = Extensions::new();
-    /// ext.insert(String::from("Hello"));
-    /// ext.get_mut::<String>().unwrap().push_str(" World");
-    ///
-    /// assert_eq!(ext.get::<String>().unwrap(), "Hello World");
-    /// ```
     pub fn get_mut<T: 'static>(&mut self) -> Option<&mut T> {
         self.map
             .as_mut()
@@ -101,14 +73,6 @@ impl Extensions {
             .and_then(|boxed| (**boxed).as_any_mut().downcast_mut())
     }
 
-    /// Get a mutable reference to a type, inserting the value created by `f` if not already present on this [`Extensions`]
-    /// # Examples
-    /// ```
-    /// let mut ext = Extensions::new();
-    /// *ext.get_or_insert_with(|| 1i32) += 2;
-    ///
-    /// assert_eq!(*ext.get::<i32>().unwrap(), 3);
-    /// ```
     #[allow(clippy::missing_panics_doc)]
     pub fn get_or_insert_with<T: Clone + Send + Sync + 'static, F: FnOnce() -> T>(
         &mut self,
@@ -122,39 +86,14 @@ impl Extensions {
         (**out).as_any_mut().downcast_mut().unwrap()
     }
 
-    /// Get a mutable reference to a type, inserting `value` if not already present on this [`Extensions`]
-    /// # Examples
-    /// ```
-    /// let mut ext = Extensions::new();
-    /// *ext.get_or_insert(1i32) += 2;
-    ///
-    /// assert_eq!(*ext.get::<i32>().unwrap(), 3);
-    /// ```
     pub fn get_or_insert<T: Clone + Send + Sync + 'static>(&mut self, value: T) -> &mut T {
         self.get_or_insert_with(|| value)
     }
 
-    /// Get a mutable reference to a type, inserting the type's default value if not already present on this [`Extensions`]
-    /// # Examples
-    /// ```
-    /// let mut ext = Extensions::new();
-    /// *ext.get_or_insert_default::<i32>() += 2;
-    ///
-    /// assert_eq!(*ext.get::<i32>().unwrap(), 2);
-    /// ```
     pub fn get_or_insert_default<T: Default + Clone + Send + Sync + 'static>(&mut self) -> &mut T {
         self.get_or_insert_with(T::default)
     }
 
-    /// Remove a type from this [`Extensions`].
-    /// If a extension of this type existed, it will be returned.
-    /// # Examples
-    /// ```
-    /// let mut ext = Extensions::new();
-    /// ext.insert(5i32);
-    /// assert_eq!(ext.remove::<i32>(), Some(5i32));
-    /// assert!(ext.get::<i32>().is_none());
-    /// ```
     pub fn remove<T: 'static>(&mut self) -> Option<T> {
         self.map
             .as_mut()
@@ -162,15 +101,6 @@ impl Extensions {
             .and_then(|boxed| boxed.into_any().downcast().ok().map(|boxed| *boxed))
     }
 
-    /// Clear the [`Extensions`] of all inserted extensions
-    /// # Examples
-    /// ```
-    /// let mut ext = Extensions::new();
-    /// ext.insert(5i32);
-    /// ext.clear();
-    ///
-    /// assert!(ext.get::<i32>().is_none());
-    /// ```
     #[inline]
     pub fn clear(&mut self) {
         if let Some(ref mut map) = self.map {
@@ -178,52 +108,18 @@ impl Extensions {
         }
     }
 
-    /// Check whether the extension set is empty or not
-    /// # Examples
-    /// ```
-    /// let mut ext = Extensions::new();
-    /// assert!(ext.is_empty());
-    /// ext.insert(5i32);
-    /// assert!(!ext.is_empty());
-    /// ```
     #[inline]
     #[must_use]
     pub fn is_empty(&self) -> bool {
         self.map.as_ref().map_or(true, |map| map.is_empty())
     }
 
-    /// Get the number of extensions available
-    /// # Examples
-    /// ```
-    /// let mut ext = Extensions::new();
-    /// assert_eq!(ext.len(), 0);
-    /// ext.insert(5i32);
-    /// assert_eq!(ext.len(), 1);
-    /// ```
     #[inline]
     #[must_use]
     pub fn len(&self) -> usize {
         self.map.as_ref().map_or(0, |map| map.len())
     }
 
-    /// Extends `self` with another [`Extensions`].
-    /// If an instance of a specific type exists in both, the one in `self` is overwritten with the one from `other`.
-    /// # Examples
-    /// ```
-    /// let mut ext_a = Extensions::new();
-    /// ext_a.insert(8u8);
-    /// ext_a.insert(16u16);
-    ///
-    /// let mut ext_b = Extensions::new();
-    /// ext_b.insert(4u8);
-    /// ext_b.insert("hello");
-    ///
-    /// ext_a.extend(ext_b);
-    /// assert_eq!(ext_a.len(), 3);
-    /// assert_eq!(ext_a.get::<u8>(), Some(&4u8));
-    /// assert_eq!(ext_a.get::<u16>(), Some(&16u16));
-    /// assert_eq!(ext_a.get::<&'static str>().copied(), Some("hello"));
-    /// ```
     pub fn extend(&mut self, other: Self) {
         if let Some(other) = other.map {
             if let Some(map) = &mut self.map {
