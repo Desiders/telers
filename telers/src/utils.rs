27aa32702b09a707c35af499845d52adc3ac84cf
Desiders <abc_desi@proton.me>
2025-03-09 16:15:50 +0300

    Add graceful shutdown features and default signal

--- telers/src/utils.rs
+++ telers/src/utils.rs
@@ -4,3 +4,9 @@ pub(crate) use error::format_error_report;
 
 pub mod text;
 pub mod token;
+
+#[cfg(feature = "signal")]
+pub mod signal;
+
+#[cfg(feature = "signal")]
+pub use signal::shutdown_signal;
