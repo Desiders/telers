a93e3647b988f4203da04779437a5cf0c7c1f81c
Desiders <abc_desi@proton.me>
2025-06-30 14:01:03 +0300

    Add `cover` and `start_timestamp` for `SendVideo`

--- telers/src/methods/send_video.rs
+++ telers/src/methods/send_video.rs
@@ -32,6 +32,10 @@ pub struct SendVideo<'a> {
     pub height: Option<i64>,
     /// Thumbnail of the file sent; can be ignored if thumbnail generation for the file is supported server-side. The thumbnail should be in JPEG format and less than 200 kB in size. A thumbnail's width and height should not exceed 320. Ignored if the file is not uploaded using `multipart/form-data`. Thumbnails can't be reused and can be only uploaded as a new file, so you can pass `attach://<file_attach_name>` if the thumbnail was uploaded using `multipart/form-data` under <file_attach_name>. [`More information on Sending Files`](https://core.telegram.org/bots/api#sending-files).
     pub thumbnail: Option<InputFile<'a>>,
+    /// Cover for the video in the message. Pass a `file_id` to send a file that exists on the Telegram servers (recommended), pass an HTTP URL for Telegram to get a file from the Internet, or pass `attach://<file_attach_name>` to upload a new one using `multipart/form-data` under `<file_attach_name>` name. [`More information on Sending Files`](https://core.telegram.org/bots/api#sending-files).
+    pub cover: Option<InputFile<'a>>,
+    /// Start timestamp for the video in the message
+    pub start_timestamp: Option<i64>,
     /// Video caption (may also be used when resending videos by `file_id`), 0-1024 characters after entities parsing
     pub caption: Option<String>,
     /// Mode for parsing entities in the video caption. See [`formatting options`](https://core.telegram.org/bots/api#formatting-options) for more details.
@@ -70,6 +74,8 @@ impl<'a> SendVideo<'a> {
             width: None,
             height: None,
             thumbnail: None,
+            cover: None,
+            start_timestamp: None,
             caption: None,
             parse_mode: None,
             caption_entities: None,
@@ -145,6 +151,22 @@ impl<'a> SendVideo<'a> {
     pub fn thumbnail(self, val: impl Into<InputFile<'a>>) -> Self {
         Self {
             thumbnail: Some(val.into()),
+            ..self
+        }
+    }
+
+    #[must_use]
+    pub fn cover(self, val: impl Into<InputFile<'a>>) -> Self {
+        Self {
+            cover: Some(val.into()),
+            ..self
+        }
+    }
+
+    #[must_use]
+    pub fn start_timestamp(self, val: i64) -> Self {
+        Self {
+            start_timestamp: Some(val),
             ..self
         }
     }
@@ -308,6 +330,22 @@ impl<'a> SendVideo<'a> {
     pub fn thumbnail_option(self, val: Option<impl Into<InputFile<'a>>>) -> Self {
         Self {
             thumbnail: val.map(Into::into),
+            ..self
+        }
+    }
+
+    #[must_use]
+    pub fn cover_option(self, val: Option<impl Into<InputFile<'a>>>) -> Self {
+        Self {
+            cover: val.map(Into::into),
+            ..self
+        }
+    }
+
+    #[must_use]
+    pub fn start_timestamp_option(self, val: Option<i64>) -> Self {
+        Self {
+            start_timestamp: val,
             ..self
         }
     }
