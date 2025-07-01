b10106a09991751884266c8c9577ffcc16a0ce39
Desiders <abc_desi@proton.me>
2025-07-01 17:06:17 +0300

    Add `SetBusinessAccountUsername`

---
+++ telers/src/methods/set_business_account_username.rs
@@ -0,0 +1,71 @@
+use super::base::{Request, TelegramMethod};
+
+use crate::client::Bot;
+
+use serde::Serialize;
+use serde_with::skip_serializing_none;
+
+/// Changes the username of a managed business account. Requires the `can_change_username` business bot right.
+/// # Documentation
+/// <https://core.telegram.org/bots/api#setbusinessaccountusername>
+/// # Returns
+/// On success, `true` is returned
+#[skip_serializing_none]
+#[derive(Debug, Clone, Hash, PartialEq, Eq, Serialize)]
+pub struct SetBusinessAccountUsername {
+    /// Unique identifier of the business connection
+    pub business_connection_id: String,
+    /// The new value of the username for the business account; 0-32 characters
+    pub username: Option<String>,
+}
+
+impl SetBusinessAccountUsername {
+    #[must_use]
+    pub fn new(business_connection_id: impl Into<String>) -> Self {
+        Self {
+            business_connection_id: business_connection_id.into(),
+            username: None,
+        }
+    }
+
+    #[must_use]
+    pub fn business_connection_id(self, val: impl Into<String>) -> Self {
+        Self {
+            business_connection_id: val.into(),
+            ..self
+        }
+    }
+
+    #[must_use]
+    pub fn username(self, val: impl Into<String>) -> Self {
+        Self {
+            username: Some(val.into()),
+            ..self
+        }
+    }
+}
+
+impl SetBusinessAccountUsername {
+    #[must_use]
+    pub fn username_option(self, val: Option<impl Into<String>>) -> Self {
+        Self {
+            username: val.map(Into::into),
+            ..self
+        }
+    }
+}
+
+impl TelegramMethod for SetBusinessAccountUsername {
+    type Method = Self;
+    type Return = bool;
+
+    fn build_request<Client>(&self, _bot: &Bot<Client>) -> Request<Self::Method> {
+        Request::new("setBusinessAccountUsername", self, None)
+    }
+}
+
+impl AsRef<SetBusinessAccountUsername> for SetBusinessAccountUsername {
+    fn as_ref(&self) -> &Self {
+        self
+    }
+}
