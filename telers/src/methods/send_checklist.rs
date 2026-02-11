7ea2020b9fc27a5fdc2b03e0be1d6e5bdacb858f
Desiders <47452083+Desiders@users.noreply.github.com>
2026-02-11 18:01:38 +0300

    Add `SendChecklist`

---
+++ telers/src/methods/send_checklist.rs
@@ -0,0 +1,176 @@
+use super::base::{Request, TelegramMethod};
+
+use crate::{
+    client::Bot,
+    types::{InlineKeyboardMarkup, InputChecklist, Message, ReplyParameters},
+};
+
+use serde::Serialize;
+use serde_with::skip_serializing_none;
+
+/// Use this method to send a checklist on behalf of a connected business account.
+/// # Documentation
+/// <https://core.telegram.org/bots/api#sendchecklist>
+/// # Returns
+/// On success, the sent [`Message`] is returned
+#[skip_serializing_none]
+#[derive(Debug, Clone, Hash, PartialEq, Eq, Serialize)]
+pub struct SendChecklist {
+    /// Unique identifier of the business connection on behalf of which the message will be sent
+    pub business_connection_id: String,
+    /// Unique identifier for the target chat
+    pub chat_id: i64,
+    /// A JSON-serialized object for the checklist to send
+    pub checklist: InputChecklist,
+    /// Sends the message silently. Users will receive a notification with no sound.
+    pub disable_notification: Option<bool>,
+    /// Protects the contents of the sent message from forwarding and saving
+    pub protect_content: Option<bool>,
+    /// Unique identifier of the message effect to be added to the message
+    pub message_effect_id: Option<String>,
+    /// A JSON-serialized object for description of the message to reply to
+    pub reply_parameters: Option<ReplyParameters>,
+    /// A JSON-serialized object for an [inline keyboard](https://core.telegram.org/bots/features#inline-keyboards).
+    pub reply_markup: Option<InlineKeyboardMarkup>,
+}
+
+impl SendChecklist {
+    #[must_use]
+    pub fn new(
+        business_connection_id: impl Into<String>,
+        chat_id: i64,
+        checklist: InputChecklist,
+    ) -> Self {
+        Self {
+            business_connection_id: business_connection_id.into(),
+            chat_id,
+            checklist,
+            disable_notification: None,
+            protect_content: None,
+            message_effect_id: None,
+            reply_parameters: None,
+            reply_markup: None,
+        }
+    }
+
+    #[must_use]
+    pub fn business_connection_id(self, val: impl Into<String>) -> Self {
+        Self {
+            business_connection_id: val.into(),
+            ..self
+        }
+    }
+
+    #[must_use]
+    pub fn chat_id(self, val: i64) -> Self {
+        Self {
+            chat_id: val,
+            ..self
+        }
+    }
+
+    #[must_use]
+    pub fn checklist(self, val: InputChecklist) -> Self {
+        Self {
+            checklist: val,
+            ..self
+        }
+    }
+
+    #[must_use]
+    pub fn disable_notification(self, val: bool) -> Self {
+        Self {
+            disable_notification: Some(val),
+            ..self
+        }
+    }
+
+    #[must_use]
+    pub fn protect_content(self, val: bool) -> Self {
+        Self {
+            protect_content: Some(val),
+            ..self
+        }
+    }
+
+    #[must_use]
+    pub fn message_effect_id(self, val: impl Into<String>) -> Self {
+        Self {
+            message_effect_id: Some(val.into()),
+            ..self
+        }
+    }
+
+    #[must_use]
+    pub fn reply_parameters(self, val: ReplyParameters) -> Self {
+        Self {
+            reply_parameters: Some(val),
+            ..self
+        }
+    }
+
+    #[must_use]
+    pub fn reply_markup(self, val: impl Into<InlineKeyboardMarkup>) -> Self {
+        Self {
+            reply_markup: Some(val.into()),
+            ..self
+        }
+    }
+}
+
+impl SendChecklist {
+    #[must_use]
+    pub fn disable_notification_option(self, val: Option<bool>) -> Self {
+        Self {
+            disable_notification: val,
+            ..self
+        }
+    }
+
+    #[must_use]
+    pub fn protect_content_option(self, val: Option<bool>) -> Self {
+        Self {
+            protect_content: val,
+            ..self
+        }
+    }
+
+    #[must_use]
+    pub fn message_effect_id_option(self, val: Option<impl Into<String>>) -> Self {
+        Self {
+            message_effect_id: val.map(Into::into),
+            ..self
+        }
+    }
+
+    #[must_use]
+    pub fn reply_parameters_option(self, val: Option<ReplyParameters>) -> Self {
+        Self {
+            reply_parameters: val,
+            ..self
+        }
+    }
+
+    #[must_use]
+    pub fn reply_markup_option(self, val: Option<impl Into<InlineKeyboardMarkup>>) -> Self {
+        Self {
+            reply_markup: val.map(Into::into),
+            ..self
+        }
+    }
+}
+
+impl TelegramMethod for SendChecklist {
+    type Method = Self;
+    type Return = Message;
+
+    fn build_request<Client>(&'_ self, _bot: &Bot<Client>) -> Request<'_, Self::Method> {
+        Request::new("sendChecklist", self, None)
+    }
+}
+
+impl AsRef<SendChecklist> for SendChecklist {
+    fn as_ref(&self) -> &Self {
+        self
+    }
+}
